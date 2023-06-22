c744f9b085fcd776ee8577cadc443096a825a909
jreid757 <jcreid1@gmail.com>
2023-06-22 22:50:45 +0000

    Hashicorp Configuration Language Lab commit

--- main.tf
+++ main.tf
@@ -101,25 +101,15 @@ resource "aws_nat_gateway" "nat_gateway" {
     Name = "demo_nat_gateway"
   }
 }
-# Terraform Data Block - To Lookup Latest Ubuntu 20.04 AMI Image
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  owners = ["099720109477"]
-}
-# Terraform Resource Block - To Build EC2 instance in Public Subnet
-resource "aws_instance" "web_server" {
-  ami           = data.aws_ami.ubuntu.id
+
+resource "aws_instance" "web" {
+  ami           = "ami-022e1a32d3f742bd8"
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+
+  subnet_id              = "subnet-02623b51189fdac6c"
+  vpc_security_group_ids = ["sg-0317dc3ff49321679"]
+
   tags = {
-    Name = "Ubuntu EC2 Server"
+    "Terraform" = "true"
   }
 }
