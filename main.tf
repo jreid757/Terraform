93f743a887e8fc1495ff5175eac357daca146c35
jreid757 <jcreid1@gmail.com>
2023-06-22 20:06:21 +0000

    benefits of terraform state 2nd commit

--- main.tf
+++ main.tf
@@ -2,40 +2,18 @@
 provider "aws" {
   region = "us-east-1"
 }
-<<<<<<< HEAD
-
-#Retrieve the list of AZs in the current AWS region
-data "aws_availability_zones" "available" {}
-data "aws_region" "current" {}
-
-#Define the VPC 
-resource "aws_vpc" "vpc" {
-  cidr_block = var.vpc_cidr
-
-=======
 #Retrieve the list of AZs in the current AWS region
 data "aws_availability_zones" "available" {}
 data "aws_region" "current" {}
 #Define the VPC
 resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr
->>>>>>> c0f6b66 (benefits of terraform state)
   tags = {
     Name        = var.vpc_name
     Environment = "demo_environment"
     Terraform   = "true"
   }
 }
-<<<<<<< HEAD
-
-#Deploy the private subnets
-resource "aws_subnet" "private_subnets" {
-  for_each          = var.private_subnets
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value)
-  availability_zone = tolist(data.aws_availability_zones.available.names)[each.value]
-
-=======
 #Deploy the private subnets
 resource "aws_subnet" "private_subnets" {
   for_each   = var.private_subnets
@@ -43,23 +21,11 @@ resource "aws_subnet" "private_subnets" {
   cidr_block = cidrsubnet(var.vpc_cidr, 8, each.value)
   availability_zone = tolist(data.aws_availability_zones.available.names)[
   each.value]
->>>>>>> c0f6b66 (benefits of terraform state)
   tags = {
     Name      = each.key
     Terraform = "true"
   }
 }
-<<<<<<< HEAD
-
-#Deploy the public subnets
-resource "aws_subnet" "public_subnets" {
-  for_each                = var.public_subnets
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = cidrsubnet(var.vpc_cidr, 8, each.value + 100)
-  availability_zone       = tolist(data.aws_availability_zones.available.names)[each.value]
-  map_public_ip_on_launch = true
-
-=======
 #Deploy the public subnets
 resource "aws_subnet" "public_subnets" {
   for_each   = var.public_subnets
@@ -68,23 +34,14 @@ resource "aws_subnet" "public_subnets" {
   availability_zone = tolist(data.aws_availability_zones.available.
   names)[each.value]
   map_public_ip_on_launch = true
->>>>>>> c0f6b66 (benefits of terraform state)
   tags = {
     Name      = each.key
     Terraform = "true"
   }
 }
-<<<<<<< HEAD
-
 #Create route tables for public and private subnets
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.vpc.id
-
-=======
-#Create route tables for public and private subnets
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.vpc.id
->>>>>>> c0f6b66 (benefits of terraform state)
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.internet_gateway.id
@@ -95,21 +52,11 @@ resource "aws_route_table" "public_route_table" {
     Terraform = "true"
   }
 }
-<<<<<<< HEAD
-
-resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    # gateway_id     = aws_internet_gateway.internet_gateway.id
-=======
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.vpc.id
   route {
     cidr_block = "0.0.0.0/0"
     # gateway_id = aws_internet_gateway.internet_gateway.id
->>>>>>> c0f6b66 (benefits of terraform state)
     nat_gateway_id = aws_nat_gateway.nat_gateway.id
   }
   tags = {
@@ -117,10 +64,6 @@ resource "aws_route_table" "private_route_table" {
     Terraform = "true"
   }
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> c0f6b66 (benefits of terraform state)
 #Create route table associations
 resource "aws_route_table_association" "public" {
   depends_on     = [aws_subnet.public_subnets]
@@ -128,20 +71,12 @@ resource "aws_route_table_association" "public" {
   for_each       = aws_subnet.public_subnets
   subnet_id      = each.value.id
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> c0f6b66 (benefits of terraform state)
 resource "aws_route_table_association" "private" {
   depends_on     = [aws_subnet.private_subnets]
   route_table_id = aws_route_table.private_route_table.id
   for_each       = aws_subnet.private_subnets
   subnet_id      = each.value.id
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> c0f6b66 (benefits of terraform state)
 #Create Internet Gateway
 resource "aws_internet_gateway" "internet_gateway" {
   vpc_id = aws_vpc.vpc.id
@@ -149,10 +84,6 @@ resource "aws_internet_gateway" "internet_gateway" {
     Name = "demo_igw"
   }
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> c0f6b66 (benefits of terraform state)
 #Create EIP for NAT Gateway
 resource "aws_eip" "nat_gateway_eip" {
   domain     = "vpc"
@@ -161,10 +92,6 @@ resource "aws_eip" "nat_gateway_eip" {
     Name = "demo_igw_eip"
   }
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> c0f6b66 (benefits of terraform state)
 #Create NAT Gateway
 resource "aws_nat_gateway" "nat_gateway" {
   depends_on    = [aws_subnet.public_subnets]
@@ -173,29 +100,26 @@ resource "aws_nat_gateway" "nat_gateway" {
   tags = {
     Name = "demo_nat_gateway"
   }
-<<<<<<< HEAD
-=======
 }
 # Terraform Data Block - To Lookup Latest Ubuntu 20.04 AMI Image
 data "aws_ami" "ubuntu" {
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  owners = ["099720109477"]
+most_recent = true
+filter {
+name = "name"
+values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+}
+filter {
+name = "virtualization-type"
+values = ["hvm"]
+}
+owners = ["099720109477"]
 }
 # Terraform Resource Block - To Build EC2 instance in Public Subnet
 resource "aws_instance" "web_server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
-  tags = {
-    Name = "Ubuntu EC2 Server"
-  }
->>>>>>> c0f6b66 (benefits of terraform state)
+ami = data.aws_ami.ubuntu.id
+instance_type = "t2.micro"
+subnet_id = aws_subnet.public_subnets["public_subnet_1"].id
+tags = {
+Name = "Ubuntu EC2 Server"
+}
 }
